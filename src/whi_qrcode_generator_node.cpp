bb2d84dc2b3a043fa76c6bc53dd8b72138afde3c
Xinjue <hit_zou@126.com>
2024-10-04 12:10:19 +0800

    add aruco support

--- src/whi_qrcode_generator_node.cpp
+++ src/whi_qrcode_generator_node.cpp
@@ -75,12 +75,13 @@ int main(int argc, char** argv)
 	if (type == "aruco")
 	{
 		int id = atoi(contents.c_str());
-		std::map<int, std::vector<int>> mapDict{
-				{4, {cv::aruco::DICT_4X4_50, cv::aruco::DICT_4X4_100, cv::aruco::DICT_4X4_250, cv::aruco::DICT_4X4_1000}},
-				{5, {cv::aruco::DICT_5X5_50, cv::aruco::DICT_5X5_100, cv::aruco::DICT_5X5_250, cv::aruco::DICT_5X5_1000}},
-				{6, {cv::aruco::DICT_6X6_50, cv::aruco::DICT_6X6_100, cv::aruco::DICT_6X6_250, cv::aruco::DICT_6X6_1000}},
-				{7, {cv::aruco::DICT_7X7_50, cv::aruco::DICT_7X7_100, cv::aruco::DICT_7X7_250, cv::aruco::DICT_7X7_1000}}
-			};
+		std::map<int, std::vector<int>> mapDict
+		{
+			{4, {cv::aruco::DICT_4X4_50, cv::aruco::DICT_4X4_100, cv::aruco::DICT_4X4_250, cv::aruco::DICT_4X4_1000}},
+			{5, {cv::aruco::DICT_5X5_50, cv::aruco::DICT_5X5_100, cv::aruco::DICT_5X5_250, cv::aruco::DICT_5X5_1000}},
+			{6, {cv::aruco::DICT_6X6_50, cv::aruco::DICT_6X6_100, cv::aruco::DICT_6X6_250, cv::aruco::DICT_6X6_1000}},
+			{7, {cv::aruco::DICT_7X7_50, cv::aruco::DICT_7X7_100, cv::aruco::DICT_7X7_250, cv::aruco::DICT_7X7_1000}}
+		};
 		int dict = cv::aruco::DICT_4X4_50;
 		if (id >= 50 && id < 100)
 		{
